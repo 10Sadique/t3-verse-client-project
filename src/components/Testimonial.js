275e5cb4b32697978aa884c05e6271f936e8a0dc
10Sadique <jafarsjahan@gmail.com>
2022-10-25 19:10:42 +0600

    footer added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -9,7 +9,7 @@ import spotify from '../assets/spotify.png';
 
 const Testimonial = () => {
     return (
-        <div className="grid grid-cols-3 md:grid-cols-6 gap-10">
+        <div className="grid grid-cols-3 md:grid-cols-6 gap-10 mb-32 mt-40">
             <img className="w-14 mx-auto" src={amazon} alt="" />
             <img className="w-14 mx-auto" src={amd} alt="" />
             <img className="w-14 mx-auto" src={cisco} alt="" />
