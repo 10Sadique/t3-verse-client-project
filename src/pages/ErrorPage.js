f12f059c531d57d0766d97f176e5d67568a99d2b
10Sadique <jafarsjahan@gmail.com>
2022-10-26 15:17:28 +0600

    error page updated and depolyed to firebase

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div className="grid h-screen place-items-center">
-            <div className="flex items-center gap-5 divide-x-4">
-                <h1 className="text-6xl font-bold">404</h1>
-                <p className="text-6xl text-gray-500">Page Not Found</p>
+            <div className="flex flex-col items-center gap-10">
+                <div className="flex items-center gap-5 divide-x-4">
+                    <h1 className="text-6xl font-bold">404</h1>
+                    <p className="text-6xl text-gray-500">Page Not Found</p>
+                </div>
+                <Link to={`/`}>
+                    <button className="px-5 py-3 font-semibold text-white bg-indigo-600 rounded-lg shadow-md">
+                        Go Back to Home
+                    </button>
+                </Link>
             </div>
         </div>
     );
